0019e3246988a9ae055e64763e5ae944003b45aa
Roel Magdaleno <roelmagdaleno@gmail.com>
2022-12-27 11:46:33 -0600

    FIX: Wrong language value when using pipes

--- admin/js/syntax-highlighting.asset.php
+++ admin/js/syntax-highlighting.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-hooks'), 'version' => '3088a18de2d48ad0c2f3');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-hooks'), 'version' => '9f5d0f5eab61b2f1083c');
